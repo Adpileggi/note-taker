928f6278140a33f1df49612a346bf90cf8cfd05b
Anthony Pileggi <adpileggi@gmail.com>
2024-03-03 19:20:47 -0500

    added delete feature

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -7,7 +7,7 @@ const readFromFile = util.promisify(fs.readFile);
 // write data to the JSON file
 const writeToFile = (destination, content) =>
     fs.writeFile(destination, JSON.stringify(content, null, 4), err =>
-    err ? console.error(err) : console.info(`\nDatat written to ${destination}`)
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
     )
 
 // function to read data from a file and append new data
